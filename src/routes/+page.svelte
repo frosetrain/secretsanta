81b2d77c6d22e3d42a7d3a0b61190809b636cbc6
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2024-12-20 09:43:42 +0800

    Sort homepage people

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
     import relativeTime from "dayjs/plugin/relativeTime";
 
     let { data } = $props();
+    data.people.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
     let ParticlesComponent: any = $state(() => {});
     onMount(async () => {
@@ -41,7 +42,7 @@
                     href="/profile"
                     class="ml-auto flex w-fit items-center gap-2 rounded-full bg-emerald-600 p-2 pr-3 font-medium text-white ring-emerald-500/60 transition hover:bg-emerald-500 active:ring-4"
                 >
-                    <img src={data.user.user_metadata.avatar_url} class="size-8 rounded-full" />
+                    <img src={data.user.user_metadata.avatar_url} alt="avatar" class="size-8 rounded-full" />
                     <p>Edit Profile</p>
                 </a>
             {:else}
@@ -55,21 +56,25 @@
     </div>
     <div class="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
         {#each data.people as person}
-            <div class="rounded bg-gray-200 p-6 shadow-md transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-gray-600/60">
-                <div class="mb-4 flex items-center gap-4">
-                    <img
-                        src="/avatar.png"
-                        alt="User avatar"
-                        class="size-10 rounded-full shadow-lg shadow-emerald-500/50 ring-2 ring-emerald-500 ring-offset-2"
-                    />
-                    <div>
-                        <p class="text-lg font-bold">{person.name}</p>
-                        <p>{person.age}, {person.gender}</p>
+            <div
+                class="flex flex-col justify-between rounded bg-gray-200 p-6 shadow-md transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-gray-600/60"
+            >
+                <div>
+                    <div class="mb-4 flex items-center gap-4">
+                        <img
+                            src="/avatar.png"
+                            alt="User avatar"
+                            class="size-10 rounded-full shadow-lg shadow-emerald-500/50 ring-2 ring-emerald-500 ring-offset-2"
+                        />
+                        <div>
+                            <p class="text-lg font-bold">{person.name}</p>
+                            <p>{person.age}, {person.gender}</p>
+                        </div>
                     </div>
+                    <p class="mb-4 font-semibold italic text-gray-700">“{person.well_wish}”</p>
+                    <p>Requesting <span class="rounded bg-gray-300 px-1.5 py-0.5">{person.request}</span></p>
                 </div>
-                <p class="mb-4 font-semibold italic text-gray-700">“{person.well_wish}”</p>
-                <p>Requesting <span class="rounded bg-gray-300 px-1.5 py-0.5">{person.request}</span></p>
-                <p class="ml-auto mt-2 w-fit text-sm text-gray-700">{dayjs().to(dayjs(person.created_at))}</p>
+                <p class="ml-auto mt-2 w-fit align-text-bottom text-sm text-gray-700">{dayjs().to(dayjs(person.created_at))}</p>
             </div>
         {/each}
     </div>
